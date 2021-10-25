51ac0abc0f7fce62a1532171cf4f60b87181b580
Savindu <blsthathsara@gmail.com>
2021-10-25 20:53:27 +0530

    create brand-carosal component Signed-off-by: Savindu <blsthathsara@gmail.com>

---
+++ src/components/Brand_Carosal.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="brand-carosal">
+        <h1>about</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'brand-carosal',
+    data(){
+        return{
+            
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
